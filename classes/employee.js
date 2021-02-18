000a54af9c6f951428b133a7d4dfe5bace2338f1
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-02-17 18:17:03 -0700

    created all files and install all packages to start writing my code

---
+++ classes/employee.js
@@ -0,0 +1,19 @@
+const Inquirer = require('inquirer')
+
+
+class Employee {
+    constructor(name, id ,email) {
+    this.name = name;
+    this.id - id;
+    this.email = email;
+}
+
+    getName() {
+        
+    }
+
+
+}
+
+
+module.exports = Employee
