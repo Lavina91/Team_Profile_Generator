b6148fd5598f35deff825ef1f95131806f9a5d70
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-02-19 22:58:17 -0700

    added test for getId for employee class

--- test/employee.test.js
+++ test/employee.test.js
@@ -4,8 +4,18 @@ describe('getName', () => {
     it('should return the name of the employee', () => {
         const name = 'Mario';
 
-        const employee = new Employee.getName(name);
+        const employee = new Employee(name);
 
         expect(employee.name).toBe(name)
+    })
+});
+
+describe('getId', () => {
+    it('should return the id of the employee', () => {
+        const id = 1;
+
+        const employee = new Employee(id);
+
+        expect(employee.id).toBe(id);
     })
 })
