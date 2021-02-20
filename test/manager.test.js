a261886f913c46790f8a59528f09060ba37af8f0
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-02-19 23:50:26 -0700

    added tests for manager class

--- test/manager.test.js
+++ test/manager.test.js
@@ -1,4 +1,22 @@
-// const manager = require('../classes/manager');
+const Manager = require('../classes/manager');
 
 
-// describe('getOfficeNum')
+describe('getOfficeNum', () => {
+    it("should return the manager's office number", () => {
+        const officeNum = 11;
+
+        const manager = new Manager('Mario', 1, 'mario@gmail.com', 11)
+
+        expect(manager.officeNum).toBe(officeNum)
+    })
+});
+
+describe('getRole', () => {
+    it("should return the employees job role", () => {
+        const jobRole = 'Manager';
+
+        const manager = new Manager();
+
+        expect(manager.role).toBe(jobRole)
+    })
+})
